0ba440433bc1c75d8ee5942d25f42d9b495999a3
[Bartosz Wierzbicki] <101505041+W1erzba@users.noreply.github.com>
2023-03-07 18:33:47 +0100

    User component changed to Class based

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,21 @@
+import { Component } from 'react';
+
 import classes from './User.module.css';
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
+class User extends Component {
+	render() {
+		return <li className={classes.user}>{this.props.name}</li>;
+	}
+}
+
+/*
+Function version of component above.
+ To transform function to Class we have to import Component from react and extends our component with  it "... extends Component {" and use React build in method "render() {" besides the returning state, and when we expect props then we have to add this. key before props.name
+
+  const User = (props) => {
+	return <li className={classes.user}>{props.name}</li>;
 };
+
+*/
 
 export default User;
