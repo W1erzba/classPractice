3adaa85764e8dc71a5c481dd699499f1e51d80e0
[Bartosz Wierzbicki] <101505041+W1erzba@users.noreply.github.com>
2023-03-07 18:50:08 +0100

    Users component transformed to Class based

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,13 +1,57 @@
-import { useState } from 'react';
-import User from './User';
+import { Component } from 'react';
 
+import User from './User';
 import classes from './Users.module.css';
 
 const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' },
+	{ id: 'u1', name: 'Max' },
+	{ id: 'u2', name: 'Manuel' },
+	{ id: 'u3', name: 'Julie' },
 ];
+
+class Users extends Component {
+	constructor() {
+		super();
+		this.state = {
+			showUsers: true,
+			more: 'test',
+		};
+	}
+
+	toggleUsersHandler() {
+		// this.state.showUsers = false // NOT
+		this.setState((curState) => {
+			// allways take an object
+			return { showUsers: !curState.showUsers };
+		});
+	}
+
+	render() {
+		const usersList = (
+			<ul>
+				{DUMMY_USERS.map((user) => (
+					<User
+						key={user.id}
+						name={user.name}
+					/>
+				))}
+			</ul>
+		);
+
+		return (
+			<div className={classes.users}>
+				<button onClick={this.toggleUsersHandler.bind(this)}>
+					{this.showUsers ? 'Hide' : 'Show'} Users
+				</button>
+				{this.state.showUsers && usersList}
+			</div>
+		);
+	}
+}
+
+export default Users;
+
+/* before transformation to a Class based function 
 
 const Users = () => {
   const [showUsers, setShowUsers] = useState(true);
@@ -35,3 +79,5 @@ const Users = () => {
 };
 
 export default Users;
+
+*/
