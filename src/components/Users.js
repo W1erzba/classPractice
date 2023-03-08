517ba06a7ee54afb6e842af909327277778a667a
[Bartosz Wierzbicki] <101505041+W1erzba@users.noreply.github.com>
2023-03-08 15:18:53 +0100

    added class based error handler

--- src/components/Users.js
+++ src/components/Users.js
@@ -12,6 +12,17 @@ class Users extends Component {
 		};
 	}
 
+	componentDidUpdate() {
+		// try {
+		//	 someCodeWithMightFail()
+		// } catch (err) {
+		//   // handle error
+		// }
+		if (this.props.users.length === 0) {
+			throw new Error('No users provided');
+		}
+	}
+
 	toggleUsersHandler() {
 		// this.state.showUsers = false // NOT
 		this.setState((curState) => {
